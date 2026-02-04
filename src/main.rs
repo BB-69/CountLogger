3c2b850d937f1db4f65c033e72f7af8d714b3a2b
bNine <taming.tanaphat@gmail.com>
2026-02-04 13:47:32 +0700

    Run web service alongside for render free hosting plan

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use axum::{Router, routing::get};
 use dotenv::dotenv;
 use std::env;
+use tokio::net::TcpListener;
 
 mod bot;
 mod commands;
@@ -12,5 +14,18 @@ async fn main() {
     dotenv().ok();
     let token = env::var("DISCORD_TOKEN").expect("Missing token in .env");
 
-    bot::run(token).await;
+    tokio::spawn(async {
+        bot::run(token).await;
+    });
+
+    /* For Web Service Hosting */
+
+    let app = Router::new().route("/", get(|| async { "📊 CountLogger Online" }));
+
+    let port = std::env::var("PORT").unwrap_or("3000".to_string());
+
+    let addr = format!("0.0.0.0:{port}");
+    let listener = TcpListener::bind(&addr).await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
